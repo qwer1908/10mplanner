dc392c8a03981ef66a58e7c8da21c922208ad94f
qwer1908 <kokoko1981@naver.com>
2021-07-27 01:57:21 +0900

    animationadded

--- clock.js
+++ clock.js
@@ -1,13 +1,32 @@
 const clock = document.querySelector(".clock")
 const love = document.querySelector(".note")
-const table = document.querySelectorAll(".time")
+const table = document.querySelectorAll(".time > div")
 
 function getClock() {
     const date = new Date();
+    const hours = date.getHours();
     clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     
 }
+
+for (let i = 0; i < table.length; i++) {
+    const date = new Date();
+    const hours = date.getHours();
+    console.log(hours)
+    if ( i === hours) {
+        table[i].classList.add("animation");
+    }
+}
+
+function sentence(){
+    const date = new Date()
+    const hours = date.getHours();
+    table[i].classList.add("animation");
+
+    
+}
 setInterval(getClock, 1000)
+
 
 
 
