d16ed2868d7abf2f1f70965394a584988a967028
qwer1908 <kokoko1981@naver.com>
2021-09-08 12:56:48 +0900

    remembercolor

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg", "10.jpeg", "11.jpeg", "12.jpeg", "13.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg", "10.jpeg", "11.jpeg", "12.jpeg", "13.jpeg","14.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
--- js/schedule.js
+++ js/schedule.js
@@ -1,23 +1,38 @@
 const scheduleForm = document.querySelector(".schedule-form");
 const scheduleList = document.querySelector(".schedule-list");
 const scheduleInput = scheduleForm.querySelector("input");
+
 const SCHEDULE_KEY="scheduless"
+
 let schedules = [];
 
 
+function saveSchedules() {
+    localStorage.setItem(SCHEDULE_KEY, JSON.stringify(schedules))
+}
+
+function deleteSchedule(event) {
+    const li= event.target.parentElement;
+    li.remove();
+    schedules = schedules.filter((schedule) => schedule.id !== parseInt(li.id));
+    saveSchedules();
+}
 function paintSchedule(newSchedule){
-    
     const list = document.createElement("li");
     list.id = newSchedule.id;
     const colorcircle = document.createElement("div");
-    var abcdef = color.value;
-    colorcircle.style.backgroundColor = abcdef;
+    colorcircle.id = newSchedule.id;
+    colorcircle.style.backgroundColor = newSchedule.color;
     colorcircle.classList.add("colorcircle");
     const span = document.createElement("span");
-    
     span.innerText = newSchedule.text;
+    const button = document.createElement("button");
+    button.innerText = "X";
+ 
+    button.addEventListener("click", deleteSchedule)
     list.appendChild(colorcircle);
     list.appendChild(span);
+    list.appendChild(button);
     scheduleList.appendChild(list);
 }
 
@@ -27,10 +42,21 @@ function handleScheduleSubmit(event) {
     scheduleInput.value ="";
     const newScheduleObj = {
         text: newSchedule,
+        color: color.value,
         id: Date.now(),
     }
     schedules.push(newScheduleObj);
     paintSchedule(newScheduleObj);
+    saveSchedules();
 }
 
 scheduleForm.addEventListener("submit", handleScheduleSubmit);
+
+const savedSchedules = localStorage.getItem(SCHEDULE_KEY);
+if (savedSchedules !== null) {
+    const parsedSchedules = JSON.parse(savedSchedules);
+    schedules=parsedSchedules;
+    parsedSchedules.forEach(paintSchedule);
+}
+
+
