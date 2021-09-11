f92abcec1400e4d466db11b525f22e5d576f3604
qwer1908 <kokoko1981@naver.com>
2021-09-11 15:29:54 +0900

    mostrecent

--- js/canvas.js
+++ js/canvas.js
@@ -1,52 +1,60 @@
 
-body.addEventListener("mousedown", mouseStart); //pc
-body.addEventListener("mouseup", mouseEnd); //pc
-let newimage= '';
-
+body.addEventListener("mousedown", mouseStart); 
+body.addEventListener("mouseup", mouseEnd); 
 var pcLong = 0;
-function mouseStart() {
-    pcLong = Date.now(); //클릭한 시간 얻어옵니다
+let clickedx = '';
+let clickedy = '';
+function mouseStart(event) {
+    pcLong = Date.now();
+    clickedx = event.pageX;
+    clickedy = event.pageY;
+    console.log(clickedx,clickedy)
 };
+function showdeleteButton(event) {
+    console.log("image clicked")
+    const img = event.target;
+    const xbutton = document.createElement("button");
+    xbutton.innerText = "x";
+    img.appendChild(xbutton);
+    console.dir(img)
+}
+
 function mouseEnd(event) {
-    const xa = event.offsetX;
-    const ya = event.offsetY;
-    function dropped(event){
-        const xa = event.offsetX;
-        const ya = event.offsetY;
-        console.log(xa,ya)
-        const target = event.target;
-        console.dir(target);
-        target.style.position = "absolute";
-        target.style.marginLeft= xa + "px";
-        target.style.marginTop= ya-740 + "px";
-    }
-    
+    const xa = event.pageX;
+    const ya = event.pageY;
+    console.dir(event);    
     var result = Date.now() - pcLong;
-    if(Number(result) > 800){ //롱 클릭 발생
-    	console.log("[two_container] : [mouseup] : [LongClick]");
-    	console.log("[start] : " + pcLong);
-    	console.log("[end] : " + Date.now());
-    	console.log("[data] : " + Number(result)); 
-    	
- 
+    if(Number(result) > 800){ 
+        console.log("longclick")    	
         const img = document.createElement("img");
         const newid= Date.now();
         img.id = newid;
-        newimage = newid;
-        img.addEventListener("dragend", dropped)
-        img.src = "http://www.google.com/intl/en_com/images/logo_plain.png";
+        img.src = "https://image.shutterstock.com/shutterstock/photos/1484978876/display_1500/stock-photo-sticker-sign-button-free-icon-free-sticker-badge-tag-1484978876.jpg"
+        img.style.height = "50px";
         img.style.position = "absolute";
-
-        console.log(xa,ya)
-
         img.style.marginLeft= xa + "px";
-        img.style.marginTop= ya-740 + "px";
-    
+        img.style.marginTop= ya-820 + "px";
+        
+        
         body.appendChild(img);
-        
 
         
 
+        img.addEventListener("click", showdeleteButton)
+
+        
     }
     
 };
+
+//Web & Mobile 다이어리 호환 가능 했으면 좋겠음.              
+//monthly도 만들기
+                               
+//스티커 다운로드 화면
+// -> 스티커 고르는 아이콘 누르면 여러개 종류별로 나타나도록
+// -> 클릭하면 화면에 추가 
+//1. drag and drop 가능하게 만든다
+//2. 클릭해서 아이콘 누르면 크기 조정 가능
+//3. 클릭해서 아이콘 누르면 이미지 돌아가게 조정 가능
+//4 . 클릭해서 엑스표 누르면 삭제 가능
+//5. 늦게 추가 된 스티커카 위로 가도록  
